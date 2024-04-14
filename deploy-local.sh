6abd40edf977dc9e8e638183214aaaf804a0f2a8
Alexander Ioffe <deusaquilus@gmail.com>
2024-04-14 00:55:06 -0400

    Update release script

--- deploy-local.sh
+++ deploy-local.sh
@@ -1,2 +1,2 @@
 echo "Deploying Terpal Locally"
-./gradlew :terpal-runtime:publishToMavenLocal :terpal-plugin-kotlin:publishToMavenLocal && ./gradlew :terpal-plugin-gradle:publishToMavenLocal -Pnosign
+./gradlew :terpal-runtime:publishToMavenLocal :terpal-plugin-kotlin:publishToMavenLocal :terpal-plugin-gradle:publishToMavenLocal -Pnosign
