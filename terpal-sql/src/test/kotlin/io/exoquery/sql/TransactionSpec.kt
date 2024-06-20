d899d356735737983bb6e771931d4e4b6061f324
Alexander Ioffe <deusaquilus@gmail.com>
2024-06-20 02:34:06 -0400

    Transaction spec working

--- terpal-sql/src/test/kotlin/io/exoquery/sql/TransactionSpec.kt
+++ terpal-sql/src/test/kotlin/io/exoquery/sql/TransactionSpec.kt
@@ -1,4 +1,85 @@
 package io.exoquery.sql
 
-class TransactionSpec {
-}
+import io.exoquery.sql.examples.id
+import io.exoquery.sql.jdbc.JdbcContext
+import io.exoquery.sql.jdbc.Sql
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+import kotlinx.serialization.Serializable
+
+class TransactionSpec: FreeSpec({
+  beforeEach {
+    GlobalEmbeddedPostgres.run(
+      """
+      DELETE FROM person;
+      DELETE FROM address;
+      """
+    )
+  }
+
+  /*
+  // Scala:
+    "provides transaction support" - {
+    "success" in {
+      ctx.run(qr1.delete)
+      ctx.transaction {
+        ctx.run(qr1.insert(_.i -> 33))
+      }
+      ctx.run(qr1).map(_.i) mustEqual List(33)
+    }
+    "failure" in {
+      ctx.run(qr1.delete)
+      intercept[IllegalStateException] {
+        ctx.transaction {
+          ctx.run(qr1.insert(_.i -> 33))
+          throw new IllegalStateException
+        }
+      }
+      ctx.run(qr1).isEmpty mustEqual true
+    }
+    "nested" in {
+      ctx.run(qr1.delete)
+      ctx.transaction {
+        ctx.transaction {
+          ctx.run(qr1.insert(_.i -> 33))
+        }
+      }
+      ctx.run(qr1).map(_.i) mustEqual List(33)
+    }
+   */
+
+  val ctx by lazy { JdbcContext(GlobalEmbeddedPostgres.get().getPostgresDatabase())  }
+
+  @Serializable
+  data class Person(val id: Int, val firstName: String, val lastName: String, val age: Int)
+
+  "provides transaction support" - {
+    val joe = Person(1, "Joe", "Bloggs", 111)
+    val jack = Person(2, "Jack", "Roogs", 222)
+
+    // Note the string elements ${...} should not have quotes around them or else they are interpreted as literals
+    fun insert(p: Person) =
+      Sql("INSERT INTO person (id, firstName, lastName, age) VALUES (${p.id}, ${p.firstName}, ${p.lastName}, ${p.age})").action()
+
+
+    fun select() = Sql("SELECT id, firstName, lastName, age FROM person").queryOf<Person>()
+
+    "success" {
+      ctx.transaction {
+        ctx.run(insert(joe))
+      }
+      ctx.run(select()) shouldBe listOf(joe)
+    }
+    "failure" {
+      ctx.run(insert(joe))
+      shouldThrow<IllegalStateException> {
+        ctx.transaction {
+          ctx.run(insert(jack))
+          throw IllegalStateException()
+        }
+      }
+      ctx.run(select()) shouldBe listOf(joe)
+    }
+  }
+})
