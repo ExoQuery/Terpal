4046b6462f458db3f4400708ec7762553895c47d
Alexander Ioffe <deusaquilus@gmail.com>
2024-07-05 16:28:56 -0400

    Move terpal-sql to own repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,5 @@ includeBuild("terpal-runtime")
 includeBuild("terpal-plugin-kotlin")
 
 include("testing")
-include("terpal-sql")
 
 rootProject.name = "terpal"
--- terpal-sql/scripts/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker-compose down && docker-compose build && docker-compose run --rm --service-ports setup
