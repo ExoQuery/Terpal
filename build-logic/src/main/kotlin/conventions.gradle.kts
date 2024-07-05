009a48e57c6d415c9c4d926fa4392929ddd2e6e4
Alexander Ioffe <deusaquilus@gmail.com>
2024-07-05 16:55:12 -0400

    Bump version

--- build-logic/src/main/kotlin/conventions.gradle.kts
+++ build-logic/src/main/kotlin/conventions.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 group = "io.exoquery"
 // Everything inherits the version from here
-version = "2.0.0-0.1.1"
+version = "2.0.0-0.2.0"
 
 check("$version".isNotBlank() && version != "unspecified")
     { "invalid version $version" }
