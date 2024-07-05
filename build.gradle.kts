7133bbfeb09acc4d8ecaaf7337fcf01f2a546b60
Alexander Ioffe <deusaquilus@gmail.com>
2024-07-05 16:34:07 -0400

    Moving to 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  kotlin("jvm") version "2.0.0-RC3" apply false
+  kotlin("jvm") version "2.0.0" apply false
   id("io.github.gradle-nexus.publish-plugin") version "1.1.0" apply false
 }
 
