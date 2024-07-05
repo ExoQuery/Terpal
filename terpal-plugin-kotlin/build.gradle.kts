fa7b1e37f6f7a337350a9a072de009c544eb0179
Alexander Ioffe <deusaquilus@gmail.com>
2024-07-05 17:12:25 -0400

    Fix release dependency

--- terpal-plugin-kotlin/build.gradle.kts
+++ terpal-plugin-kotlin/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("conventions")
     id("publish")
@@ -8,9 +10,9 @@ kotlin {
     jvmToolchain(11)
 }
 
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions{
-        freeCompilerArgs = listOf("-Xcontext-receivers")
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile>().configureEach {
+    compilerOptions {
+        freeCompilerArgs.add("-Xcontext-receivers")
         // Otherwise will have: Could not resolve io.exoquery:pprint-kotlin:2.0.1.
         // Incompatible because this component declares a component, compatible with Java 11 and the consumer needed a component, compatible with Java 8
         java {
@@ -20,13 +22,36 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         // If I remove this I get:
         //  'compileJava' task (current target is 11) and 'kaptGenerateStubsKotlin' task (current target is 1.8) jvm target compatibility should be set to the same Java version.
         // Not sure why
-        jvmTarget = "11"
+        jvmTarget.set(JvmTarget.JVM_11)
     }
 }
+
+// Old way of doing it
+//tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+//    kotlinOptions{
+//        freeCompilerArgs = listOf("-Xcontext-receivers")
+//        // Otherwise will have: Could not resolve io.exoquery:pprint-kotlin:2.0.1.
+//        // Incompatible because this component declares a component, compatible with Java 11 and the consumer needed a component, compatible with Java 8
+//        java {
+//            sourceCompatibility = JavaVersion.VERSION_11
+//            targetCompatibility = JavaVersion.VERSION_11
+//        }
+//        // If I remove this I get:
+//        //  'compileJava' task (current target is 11) and 'kaptGenerateStubsKotlin' task (current target is 1.8) jvm target compatibility should be set to the same Java version.
+//        // Not sure why
+//        jvmTarget = "11"
+//    }
+//}
 
 // Version from conventions.gradle.kts
 val thisVersion = version
 //println("------------------ This Version: ${thisVersion} --------------------------")
+
+// Having issue with dokka failing: Reason: Task ':kaptmodule:dokkaHtml' uses this output of task ':kaptmodule:kaptDebugKotlin' without declaring an explicit or implicit dependency. This can lead to incorrect results being produced, depending on what order the tasks are executed.
+// See: https://github.com/Kotlin/dokka/issues/3117
+afterEvaluate {
+    tasks["dokkaHtml"].dependsOn(tasks.getByName("kaptKotlin"))
+}
 
 dependencies {
     // Looks like it knows to do a project-dependency even if there is a version attached (i.e. I guess it ignores the version?)
