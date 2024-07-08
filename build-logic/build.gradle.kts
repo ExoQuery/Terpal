8abbc7726859d6e387f9124470e315b6ffbc7aee
Alexander Ioffe <deusaquilus@gmail.com>
2024-07-08 02:44:08 -0400

    Republish 0.2.0 for 1.9.20

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 }
 
 dependencies {
-  implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
+  implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
   implementation("io.github.gradle-nexus:publish-plugin:1.1.0")
   implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.8.20")
 
