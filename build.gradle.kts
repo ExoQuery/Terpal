47ac3067cfa5dec9901ef961225e2744f07226a7
Alexander Ioffe <deusaquilus@gmail.com>
2025-06-26 11:19:11 -0400

    Try a different way to avoid concurrency (which causes ssrh-staging drops)

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,3 +10,87 @@ allprojects {
         mavenLocal()
     }
 }
+
+tasks.register("publishLinux") {
+  dependsOn(
+    gradle.includedBuild(Release.Project.`terpal-runtime`).task(":publishAllPublicationsToOssRepository"),
+    gradle.includedBuild(Release.Project.`terpal-plugin-gradle`).task(":publish"),
+    gradle.includedBuild(Release.Project.`terpal-plugin-kotlin`).task(":publish"),
+  )
+}
+
+tasks.register("publishLinuxLocal") {
+  ":${Release.Project.`terpal-runtime`}:publishToMavenLocal" // should this first clause even be here?
+  dependsOn(
+    gradle.includedBuild(Release.Project.`terpal-runtime`).task(":publishToMavenLocal"),
+    gradle.includedBuild(Release.Project.`terpal-plugin-gradle`).task(":publishToMavenLocal"),
+    gradle.includedBuild(Release.Project.`terpal-plugin-kotlin`).task(":publishToMavenLocal"),
+  )
+}
+
+tasks.register("publishMac") {
+  Release.macBuildCommands.forEach {
+    dependsOn(gradle.includedBuild(Release.Project.`terpal-runtime`).task(":$it"))
+  }
+}
+
+tasks.register("publishWindows") {
+  Release.windowsBuildCommands.forEach {
+    dependsOn(gradle.includedBuild(Release.Project.`terpal-runtime`).task(":$it"))
+  }
+}
+
+
+tasks.register("publishLinuxNoRunner") {
+  dependsOn(
+    gradle.includedBuild(Release.Project.`terpal-runtime`).task(":publishAllPublicationsToOssRepository"),
+    gradle.includedBuild(Release.Project.`terpal-plugin-gradle`).task(":publish"),
+    gradle.includedBuild(Release.Project.`terpal-plugin-kotlin`).task(":publish"),
+  )
+}
+tasks.register("publishMacNoRunner") {
+  Release.macBuildCommands.forEach {
+    dependsOn(gradle.includedBuild(Release.Project.`terpal-runtime`).task(":$it"))
+  }
+}
+tasks.register("publishWindowsNoRunner") {
+  Release.windowsBuildCommands.forEach {
+    dependsOn(gradle.includedBuild(Release.Project.`terpal-runtime`).task(":$it"))
+  }
+}
+
+
+object Release {
+
+  object Project {
+    val `terpal-runtime` = "terpal-runtime"
+    val `terpal-plugin-gradle` = "terpal-plugin-gradle"
+    val `terpal-plugin-kotlin` = "terpal-plugin-kotlin"
+  }
+
+  // :terpal-runtime:publishMacosX64PublicationToOss
+  // :terpal-runtime:publishMacosArm64PublicationToOss
+  // :terpal-runtime:publishIosX64PublicationToOss
+  // :terpal-runtime:publishIosArm64PublicationToOss
+
+  val macBuildCommands =
+    listOf(
+      //"iosX64",
+      //"iosArm64",
+      "tvosX64",
+      "tvosArm64",
+      "watchosX64",
+      "watchosArm32",
+      "watchosArm64",
+      //"macosX64",
+      //"macosArm64",
+      "iosSimulatorArm64"
+    ).map { "publish${it.capitalize()}PublicationToOssRepository" }
+
+  val windowsBuildCommands =
+    listOf(
+      "mingwX64"
+    ).map { "publish${it.capitalize()}PublicationToOssRepository" }
+
+  fun String.capitalize() = this.replaceFirstChar { it.uppercase() }
+}
